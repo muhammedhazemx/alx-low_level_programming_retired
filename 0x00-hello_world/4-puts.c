483a52bbf10c4f445d68345e6a79ebc6491bb5d8
Moamed Hazem <mohamedhazemx@outlook.com>
2023-02-20 18:41:30 -0800

    created 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to the standard output
+ * using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
