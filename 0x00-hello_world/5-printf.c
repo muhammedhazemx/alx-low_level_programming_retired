3a6661c5dad35a5de101d560a7da871b0b4a8d04
Moamed Hazem <mohamedhazemx@outlook.com>
2023-02-20 18:59:53 -0800

    created 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a message to the standard output
+ * using the printf function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+
