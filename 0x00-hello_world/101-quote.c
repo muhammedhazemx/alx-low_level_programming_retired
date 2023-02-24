9b0e189f8620cf16c945ffdf4dda22c0fd218e60
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-02-24 02:06:18 +0000

    changes to 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints to string
- * Description: Prints "and that piece of art is useful.." without puts
- * Return: 1
+ * main - prints a string
+ *
+ * Description: Prints the string "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
 	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	fprintf(stderr, "%s", s);
 
 	return (1);
