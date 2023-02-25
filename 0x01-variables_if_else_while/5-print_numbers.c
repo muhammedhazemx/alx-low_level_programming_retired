c5b832c6d7739f78ebe473c58ccbe99e1eea2f0f
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-02-25 06:33:54 +0000

    created a program that iterates over single digit base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: prints all ll single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+	return (0);
+}
