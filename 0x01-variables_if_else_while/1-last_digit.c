d8ac7b74321d247fcc968a2193ef172978987f7a
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-02-25 03:59:29 +0000

    add program  to print the last digit of an integer and its properties

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * description: prints the last digit of a random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int last_digit = n % 10;
+
+	if (last_digit > 5)
+	{
+		print("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		print("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %d is %d", n, last_digit);
+		printf("and is less than 6 and not 0\n");
+	}
+
+	return (0);
+}
