642e5910970c1940659ccf99505c5b0df94ab034
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-08 20:55:50 +0000

    created 9-times_tab;e

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times tables, starting with 0
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int row, col, product;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			product = row * col;
+			if (col == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+				/**
+				* we add '0 as it's the value is 48 in
+				* Decimal so 48 + 1 = 49 which is
+				*the value of 1 in ASCII etc..
+				*/
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0'); /* we / by 10 to get the 1st digit*/
+				_putchar(product % 10 + '0'); /* we use the % to get the 2nd digit*/
+			}
+		}
+	}
+}
