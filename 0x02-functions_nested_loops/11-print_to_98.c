a1575cab7bc0ef2261b9cbfca69382264772e16d
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-11 20:37:52 +0000

    uddated 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-		printf("\n");
+
 	}
 	else if (n > 98)
 	{
@@ -29,4 +29,5 @@ void print_to_98(int n)
 	{
 		printf("%d", n);
 	}
+	printf("98\n");
 }
