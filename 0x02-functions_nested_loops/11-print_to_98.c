19a77975cb86b24270097efcd4f049293a2044e0
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-11 19:20:15 +0000

    updated 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,29 +9,40 @@
 
 void print_to_98(int n)
 {
-	if(n < 98)
+	if (n <= 98)
 	{
-		for(; n < 98; n++)
+		for (; n < 98; n++)
 		{
-			if (n == 0)
+			if (n < 0)
+			{
+				_putchar('-');
+				_putchar(n / -10 + '0');
+				_putchar(n % 10 * -1 + '0');
+			}
+			else if (n == 0)
 			{
 				_putchar('0');
-				_putchar(',');
-			}
-			else if (n > 0 || n <= 9)
-			{
-				_putchar(' ');
-				_putchar(n + '0');
-				_putchar(',');
 			}
 			else
 			{
-				_putchar(' ');
 				_putchar(n / 10 + '0');
 				_putchar(n % 10 + '0');
-				_putchar(',');
 			}
-			
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
+	else
+	{
+		for (; n > 98; n--)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
