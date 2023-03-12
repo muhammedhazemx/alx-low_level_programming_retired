0f86cca3c6d97d768817ec3100a8208c98472727
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-12 00:20:08 +0000

    made 102-fibonacci.c betty compliant

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- * 
+ *
  * Return: 0
  */
 
@@ -10,17 +10,18 @@ int main(void)
 {
 	int count;
 	long int fib1 = 0, fib2 = 1, sum;
+
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
-		printf ("%ld", sum);
+		printf("%ld", sum);
 		fib1 = fib2;
 		fib2 = sum;
 		if (count < 49)
 		{
 			printf(", ");
 		}
-		else 
+		else
 		{
 			printf("\n");
 		}
