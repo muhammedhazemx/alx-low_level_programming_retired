0894ec06d0892356cf36d2834146f4973167b411
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-15 00:52:15 +0000

    updated main.h and added _putchar file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(void)
 
 	for(i = 0; i < 10; i++)
 	{
-		_puthcar(i + '0');
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef Main_H
 #define Main_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
