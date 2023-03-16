29444f35d1900d1839cca37062586c10d69c9d6e
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-16 02:53:37 +0000

    Added print_square function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using #pragma endregion
+ *
+ * @size: size of the length and width of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
