fb629cb3923e5c79d5d9c03007ab294a4083cdf4
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-18 21:57:13 +0000

    updated print_square

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: void
+*/
+
+void print_triangle(int size)
+{
+	int col, row, space;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (space = size - row; space > 1; space--)
+			{
+				_putchar(' ');
+			}
+			for (col = 0; col <= row; col++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
