abf05f842dc87607c9360b43b3afd73569a634da
Mohamed Hazem <95584758+muhammedhazemx@users.noreply.github.com>
2023-03-18 22:48:34 +0000

    updated fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,25 @@ int main(void)
 
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			_putchar(' ');
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
