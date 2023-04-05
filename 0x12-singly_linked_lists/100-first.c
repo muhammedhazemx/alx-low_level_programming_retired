5d6de67e50e6ae6953999a88284dce77335b1a3a
mohamed hazem <mohamedhazemx@outlook.com>
2023-04-05 21:25:47 +0000

    updated 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
-void before(void) __attribute__((constructor));
+void __attribute__((constructor)) hare(void);
 
 /**
- * before - Prints a message before main function is executed
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-void before(void)
+void hare(void)
 {
-	static int i;
-
-	++i;
 	printf("You're beat! and yet, you must allow,\n"
-		   "I bore my house upon my back!\n");
-
-	if (i == 1)
-	{
-		printf("I repeat, ");
-	}
+		   "I bore my house upon my back!\n"
+		   "(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
