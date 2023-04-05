e6afce1f5ad1dcee98e1d6e59e0bc95ad9eb974c
mohamed hazem <mohamedhazemx@outlook.com>
2023-04-05 21:33:10 +0000

    updated 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,7 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
+
 	printf("You're beat! and yet, you must allow,\n"
-		   "I bore my house upon my back!\n"
-		   "(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+		   "I bore my house upon my back!\n");
 }
